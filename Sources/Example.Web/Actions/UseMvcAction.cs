c4029e541fe52157403ddd7b1f7faabdb341bd1c
Vitaliy Grigorenko <vitaliy.grigorenko@outlook.com>
2018-06-01 09:41:22 +0300

    Add area

--- Sources/Example.Web/Actions/UseMvcAction.cs
+++ Sources/Example.Web/Actions/UseMvcAction.cs
@@ -11,6 +11,8 @@ namespace Example.Web.Actions
 
     public void Execute(IRouteBuilder routeBuilder, IServiceProvider serviceProvider)
     {
+      routeBuilder.MapRoute(name: "areas", template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
       routeBuilder.MapRoute(name: "Default", template: "{controller=Default}/{action=Index}/{id?}");
     }
   }
---
+++ Sources/Example.Web/Areas/Backend/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example.Web.Backend.Controllers
+{
+  [Area("Backend")]
+  public class HomeController : Controller
+  {
+    [HttpGet]
+    public IActionResult Index()
+    {
+      return this.Content("Areas -> Backend -> Home -> Index");
+    }
+  }
+}
